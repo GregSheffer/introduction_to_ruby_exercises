4487794850d306dc304d7bf531af3603262e3dc5
Gregory Sheffer <gregorysheffer89@gmail.com>
2017-06-23 15:04:52 -0500

    Added exercises for the Methods chapter

---
+++ Methods/greeting.rb
@@ -0,0 +1,7 @@
+def greeting name
+  puts "Hello #{name}!"
+end
+
+greeting "Greg"
+greeting "Jose"
+greeting "Kylie"
---
+++ Methods/multiply.rb
@@ -0,0 +1,7 @@
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+product = multiply(6, 8)
+
+p product
